c689ff2672d6b4ee61410e1b79780faaa85d7d9c
THIRUMALAI <sakthihack67@gmail.com>
2025-07-28 14:55:02 +0530

    Remove duplicate BrowserRouter from Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import { Routes as RouterRoutes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import ErrorBoundary from "./components/ErrorBoundary";
 import CartStateManager from "./components/ui/CartStateManager";
 // Add your imports here
 import AdminLogin from "./pages/admin-login";
@@ -13,23 +12,19 @@ import NotFound from "./pages/NotFound";
 
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <CartStateManager>
-        <ErrorBoundary>
-        <ScrollToTop />
-        <RouterRoutes>
-          {/* Define your routes here */}
-          <Route path="/" element={<CustomerMenuBrowse />} />
-          <Route path="/admin-login" element={<AdminLogin />} />
-          <Route path="/upi-payment-confirmation" element={<UpiPaymentConfirmation />} />
-          <Route path="/shopping-cart-checkout" element={<ShoppingCartCheckout />} />
-          <Route path="/customer-menu-browse" element={<CustomerMenuBrowse />} />
-          <Route path="/admin-dashboard-order-management" element={<AdminDashboardOrderManagement />} />
-          <Route path="*" element={<NotFound />} />
-        </RouterRoutes>
-        </ErrorBoundary>
-      </CartStateManager>
-    </BrowserRouter>
+    <CartStateManager>
+      <ScrollToTop />
+      <RouterRoutes>
+        {/* Define your routes here */}
+        <Route path="/" element={<CustomerMenuBrowse />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/upi-payment-confirmation" element={<UpiPaymentConfirmation />} />
+        <Route path="/shopping-cart-checkout" element={<ShoppingCartCheckout />} />
+        <Route path="/customer-menu-browse" element={<CustomerMenuBrowse />} />
+        <Route path="/admin-dashboard-order-management" element={<AdminDashboardOrderManagement />} />
+        <Route path="*" element={<NotFound />} />
+      </RouterRoutes>
+    </CartStateManager>
   );
 };
 
